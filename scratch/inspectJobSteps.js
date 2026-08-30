62ea3467011927cb1507c512bb41787f43e09e2a
Aditya Yadav <aditys4444@gmail.com>
2026-08-30 14:15:06 +0530

    Streamline APK packaging and publish to GitHub Releases

--- scratch/inspectJobSteps.js
+++ scratch/inspectJobSteps.js
@@ -1,7 +1,7 @@
 async function inspectJob() {
-  const res = await fetch('https://github.com/aditys4444/cinevault/actions/runs/33302222596');
+  const res = await fetch('https://github.com/aditys4444/cinevault/actions/runs/33302279936');
   const html = await res.text();
-  const jobLinks = html.match(/href="\/aditys4444\/cinevault\/actions\/runs\/33302222596\/job\/\d+"/g);
+  const jobLinks = html.match(/href="\/aditys4444\/cinevault\/actions\/runs\/33302279936\/job\/\d+"/g);
   console.log('Job Links:', jobLinks);
   if (jobLinks && jobLinks[0]) {
     const jobUrl = 'https://github.com' + jobLinks[0].replace('href="', '').replace('"', '');
--- scratch/watchRun.js
+++ scratch/watchRun.js
@@ -11,4 +11,4 @@ async function watchRun(runId) {
   };
   console.log('Run Status:', status);
 }
-watchRun('33302222596');
+watchRun('33302279936');
