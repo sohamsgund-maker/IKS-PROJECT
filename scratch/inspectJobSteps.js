abd60953162c73ef1c452b30e79e9bad163c207e
Aditya Yadav <aditys4444@gmail.com>
2026-08-30 14:51:20 +0530

    Upload build log and use explicit :app:assembleDebug task

--- scratch/inspectJobSteps.js
+++ scratch/inspectJobSteps.js
@@ -1,7 +1,7 @@
 async function inspectJob() {
-  const res = await fetch('https://github.com/aditys4444/cinevault/actions/runs/33303713641');
+  const res = await fetch('https://github.com/aditys4444/cinevault/actions/runs/33303780619');
   const html = await res.text();
-  const jobLinks = html.match(/href="\/aditys4444\/cinevault\/actions\/runs\/33303713641\/job\/\d+"/g);
+  const jobLinks = html.match(/href="\/aditys4444\/cinevault\/actions\/runs\/33303780619\/job\/\d+"/g);
   console.log('Job Links:', jobLinks);
   if (jobLinks && jobLinks[0]) {
     const jobUrl = 'https://github.com' + jobLinks[0].replace('href="', '').replace('"', '');
--- scratch/watchRun.js
+++ scratch/watchRun.js
@@ -11,4 +11,4 @@ async function watchRun(runId) {
   };
   console.log('Run Status:', status);
 }
-watchRun('33303713641');
+watchRun('33303780619');
